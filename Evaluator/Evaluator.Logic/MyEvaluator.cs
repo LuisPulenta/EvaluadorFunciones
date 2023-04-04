fbcb2afce24e99a00a4370606daca65c7ffab066
LuisPulenta <luisalbertonu@gmail.com>
2023-04-04 11:09:40 -0300

    Evaluator begins

---
+++ Evaluator/Evaluator.Logic/MyEvaluator.cs
@@ -0,0 +1,48 @@
+﻿namespace Evaluator.Logic
+{
+    public class MyEvaluator
+    {
+        public static double Evaluate(string infix)
+        {
+            var prefix = ToPostfix(infix);
+            return Calculate(prefix);
+        }
+
+        private static double Calculate(object postfix)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static object ToPostfix(string infix)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int PriorityInExpression(char @operator)
+        {
+            switch (@operator){
+                case '^': return 4;
+                case '*': return 2;
+                case '/': return 2;
+                case '+': return 1;
+                case '-': return 1;
+                case '(': return 5;
+                default: throw new Exception("Not valid operator");
+            }
+        }
+
+        private int PriorityInStack(char @operator)
+        {
+            switch (@operator)
+            {
+                case '^': return 3;
+                case '*': return 2;
+                case '/': return 2;
+                case '+': return 1;
+                case '-': return 1;
+                case '(': return 0;
+                default: throw new Exception("Not valid operator");
+            }
+        }
+    }
+}
---
+++ Evaluator/Evaluator.UI.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Evaluator.Logic;
+
+Console.WriteLine("Evaluador de Funciones");
+Console.WriteLine("----------------------");
+var expresion = "4*5/(4+6)";
+Console.WriteLine($"{ expresion} =  {MyEvaluator.Evaluate(expresion)}");
